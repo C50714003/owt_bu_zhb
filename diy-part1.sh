1c4b278537df15f3b37ae8a2fea150105ecf60f9
C50714003 <zhangbo88797005@126.com>
2022-09-17 19:23:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,11 +21,9 @@
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-#echo >> feeds.conf.default
+echo >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-echo 'https://github.com/linkease/istore.git' >> feeds.conf.default
-echo 'https://github.com/linkease/istore-packages.git' >> feeds.conf.default
 
 git clone https://github.com/linkease/istore.git package/istore
 git clone https://github.com/linkease/openwrt-themedog.git package/openwrt-themedog
